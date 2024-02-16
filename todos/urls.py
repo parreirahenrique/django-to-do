3f93879f5ccf37a7104d8ab46205bc554c984222
Henrique Castro <henrique_castro97@hotmail.com>
2024-02-16 18:21:51 -0300

    edit task, mark as done and mark as undone functionalities

--- todos/urls.py
+++ todos/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 from . import views
+
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    path('markAsDone/<int:pk>/', views.markAsDone, name='markAsDone'),
+    path('markAsUndone/<int:pk>/', views.markAsUndone, name='markAsUndone'),
+    path('editTask/<int:pk>/', views.editTask, name='editTask'),
 ]
--- todos/views.py
+++ todos/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Task
 
@@ -7,3 +7,27 @@ def addTask(request):
     task = request.POST['task']
     Task.objects.create(task=task)
     return redirect('home')
+
+def markAsDone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
+
+def markAsUndone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('home')
+
+def editTask (request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        task.task = request.POST['task']
+        task.save()
+        return redirect('home')
+    else:
+        context = {
+            'task': task
+        }
+        return render(request, 'edit_task.html', context)
